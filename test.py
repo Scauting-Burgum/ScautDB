135b4419399a89897ccc67977c90373e4105fed3
Thomas Egbert Duursma <thomasduursma@outlook.com>
2018-02-11 16:38:49 +0100

    Implement unit testing for Row.__setitem__

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ class TestRow(unittest.TestCase):
     def test___init__(self):
         from row import Row
         from database import Database
-        import os
 
         try:
+            import os
             os.remove('test_Row.__init__.db')
         except OSError:
             pass
@@ -27,6 +27,32 @@ class TestRow(unittest.TestCase):
 
         self.assertEqual(Row(table, 1), expectedRow)
 
+    def test___setitem__(self):
+        from row import Row
+        from database import Database
+
+        try:
+            import os
+            os.remove('test_Row.__setitem__.db')
+        except OSError:
+            pass
+
+        db = Database('test_Row.__setitem__.db')
+
+        table = db.create_table('people', [('name', 'TEXT'), ('age', 'INTEGER', 0)])
+
+        row = table.insert({'name':'Albert', 'age':13})
+
+        row['name'] = 'Joran'
+        self.assertEqual(row['name'], 'Joran')
+
+        row['age'] = 14
+        self.assertEqual(row['age'], 14)
+
+        from exceptions import MissingColumnError
+
+        with self.assertRaises(MissingColumnError):
+            row['nonexistentcolumn'] = 'A very boring value...'
 
 if __name__ == '__main__':
     unittest.main()
